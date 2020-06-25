fba86852f2c630e553070e66331a24bce9b48554
Gary Briggs <gbriggs@rand.org>
2020-06-25 15:10:56 -0700

    Reformat check ordered pairs table

--- original_results.sql
+++ original_results.sql
@@ -148,12 +148,39 @@ INSERT OR IGNORE INTO mr1418_orderedpairs (digit1, digit2, cnt) VALUES
     (9, 0, 501), (9, 1, 496), (9, 2, 536), (9, 3, 493), (9, 4, 474),
     (9, 5, 504), (9, 6, 500), (9, 7, 515), (9, 8, 461), (9, 9, 494);
 
-CREATE VIEW IF NOT EXISTS check_ordered_pair AS
+CREATE VIEW IF NOT EXISTS precursor_check_ordered_pair AS
 SELECT digitpairs_50k.digit1, digitpairs_50k.digit2, mr1418_orderedpairs.cnt AS orig_cnt, digitpairs_50k.cnt AS new_cnt,
                   (digitpairs_50k.cnt-mr1418_orderedpairs.cnt) AS abs_delta,
                   (digitpairs_50k.cnt-mr1418_orderedpairs.cnt)/CAST (mr1418_orderedpairs.cnt AS REAL) AS rel_delta
      FROM digitpairs_50k INNER JOIN mr1418_orderedpairs ON
         digitpairs_50k.digit1=mr1418_orderedpairs.digit1 AND digitpairs_50k.digit2=mr1418_orderedpairs.digit2;
+
+CREATE VIEW IF NOT EXISTS check_ordered_pair AS 
+WITH q AS (SELECT digit1
+    ,SUM(CASE WHEN digit2=0 THEN new_cnt END) AS new_cnt_0, SUM(CASE WHEN digit2=0 THEN abs_delta END) delta_0
+    ,SUM(CASE WHEN digit2=1 THEN new_cnt END) AS new_cnt_1, SUM(CASE WHEN digit2=1 THEN abs_delta END) delta_1
+    ,SUM(CASE WHEN digit2=2 THEN new_cnt END) AS new_cnt_2, SUM(CASE WHEN digit2=2 THEN abs_delta END) delta_2
+    ,SUM(CASE WHEN digit2=3 THEN new_cnt END) AS new_cnt_3, SUM(CASE WHEN digit2=3 THEN abs_delta END) delta_3
+    ,SUM(CASE WHEN digit2=4 THEN new_cnt END) AS new_cnt_4, SUM(CASE WHEN digit2=4 THEN abs_delta END) delta_4
+    ,SUM(CASE WHEN digit2=5 THEN new_cnt END) AS new_cnt_5, SUM(CASE WHEN digit2=5 THEN abs_delta END) delta_5
+    ,SUM(CASE WHEN digit2=6 THEN new_cnt END) AS new_cnt_6, SUM(CASE WHEN digit2=6 THEN abs_delta END) delta_6
+    ,SUM(CASE WHEN digit2=7 THEN new_cnt END) AS new_cnt_7, SUM(CASE WHEN digit2=7 THEN abs_delta END) delta_7
+    ,SUM(CASE WHEN digit2=8 THEN new_cnt END) AS new_cnt_8, SUM(CASE WHEN digit2=8 THEN abs_delta END) delta_8
+    ,SUM(CASE WHEN digit2=9 THEN new_cnt END) AS new_cnt_9, SUM(CASE WHEN digit2=9 THEN abs_delta END) delta_9
+       FROM precursor_check_ordered_pair GROUP BY digit1)
+SELECT digit1
+,CAST(new_cnt_0 AS INTEGER) || CASE WHEN delta_0=0 THEN '' ELSE printf(' (%+d)', delta_0) END AS digit2_0
+,CAST(new_cnt_1 AS INTEGER) || CASE WHEN delta_1=0 THEN '' ELSE printf(' (%+d)', delta_1) END AS digit2_1
+,CAST(new_cnt_2 AS INTEGER) || CASE WHEN delta_2=0 THEN '' ELSE printf(' (%+d)', delta_2) END AS digit2_2
+,CAST(new_cnt_3 AS INTEGER) || CASE WHEN delta_3=0 THEN '' ELSE printf(' (%+d)', delta_3) END AS digit2_3
+,CAST(new_cnt_4 AS INTEGER) || CASE WHEN delta_4=0 THEN '' ELSE printf(' (%+d)', delta_4) END AS digit2_4
+,CAST(new_cnt_5 AS INTEGER) || CASE WHEN delta_5=0 THEN '' ELSE printf(' (%+d)', delta_5) END AS digit2_5
+,CAST(new_cnt_6 AS INTEGER) || CASE WHEN delta_6=0 THEN '' ELSE printf(' (%+d)', delta_6) END AS digit2_6
+,CAST(new_cnt_7 AS INTEGER) || CASE WHEN delta_7=0 THEN '' ELSE printf(' (%+d)', delta_7) END AS digit2_7
+,CAST(new_cnt_8 AS INTEGER) || CASE WHEN delta_8=0 THEN '' ELSE printf(' (%+d)', delta_8) END AS digit2_8
+,CAST(new_cnt_9 AS INTEGER) || CASE WHEN delta_9=0 THEN '' ELSE printf(' (%+d)', delta_9) END AS digit2_9
+FROM q ORDER BY digit1;
+
 
 -- Table 6 "Run Test"
 CREATE TABLE IF NOT EXISTS mr1418_runs (runlen INTEGER NOT NULL, expected_cnt REAL NOT NULL, cnt INTEGER NOT NULL, UNIQUE(runlen));
