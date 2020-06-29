4fb75297f2b2cda7e98703be2ac805b6a1a7dc24
Gary Briggs <gbriggs@rand.org>
2020-06-29 15:50:50 -0700

    Find examples where moving one card would generate the sequencing error

--- punchcards.sql
+++ punchcards.sql
@@ -18,8 +18,17 @@ digits_iter(startid, endid, cardnum, cardwidth, card_digits, len) AS
 cards AS (SELECT startid, endid, cardnum, cardwidth, card_digits FROM digits_iter WHERE len=cardwidth)
 SELECT * FROM cards;
 
+
 -- How could we switch two cards to fix the sequencing error?
-CREATE VIEW switch_cards_sequencing AS
+-- Find two cards with the same first digit
+--   - One card ends with a 4 with 1 on the next card
+--   - Another card that ends with a 9 with a 4 on the next card
+-- Swap them.
+-- There's a parallel version where the change is at the start of a card,
+--   but overall "switch two cards and everything else is the same" is
+--   vanishingly unlikely compared to the next approach.
+
+CREATE VIEW sequence_switch_cards AS
 WITH pc_50k AS (SELECT * FROM punchcards WHERE endid<=50000),
          my_cards AS (SELECT pc_50k.*,
     SUBSTR(LAG(pc_50k.card_digits) OVER(PARTITION BY pc_50k.cardwidth ORDER BY pc_50k.startid ASC), pc_50k.cardwidth)  AS prev_last,
@@ -37,4 +46,48 @@ SELECT csc_1.cardwidth, csc_1.cardnum AS card1, csc_1.prev_last AS prev_last_car
                AND csc_1.prev_last=csc_2.prev_last AND csc_1.last<csc_2.last
 ORDER BY card1;
 
+
+-- Fix a sequencing error with a single card move:
+-- Most plausible story is one card out of order, so look for
+--   where the group of cards could be moved along
+
+-- Category 1:
+-- xxxx9 1x/x4 4xxxx xxxx4 4xxxx 
+--         ^--- take one or more adjacent cards
+--                        ^--- insert here
+-- Pairs 91 44 44 become 94 41 44
+
+-- Category 2:
+-- xxxx4 4x/x9 1xxxx xxxx4 4xxxx 
+--         ^--- take one or more adjacent cards
+--                        ^--- insert here
+-- Pairs 44 91 44 becomes 41 44 94
+
+CREATE VIEW sequence_move_one_card AS
+WITH pc_50k AS (SELECT * FROM punchcards WHERE endid<=50000),
+         my_cards AS (SELECT pc_50k.*,
+    SUBSTR(LAG(pc_50k.card_digits) OVER(PARTITION BY pc_50k.cardwidth ORDER BY pc_50k.startid ASC), pc_50k.cardwidth)  AS prev_last,
+    SUBSTR(pc_50k.card_digits, 1, 1) AS first,
+    SUBSTR(pc_50k.card_digits, pc_50k.cardwidth) AS last,
+    SUBSTR(LEAD(pc_50k.card_digits) OVER(PARTITION BY pc_50k.cardwidth ORDER BY pc_50k.startid ASC), pc_50k.cardwidth) AS next_first
+    FROM pc_50k),
+  candidate_insertpoints AS (SELECT * FROM my_cards WHERE prev_last='4' AND first='4'),
+  candidate_sequencing_cat1 AS (SELECT 'cat1' AS category, cardwidth,
+                       (prev_last='9' AND first='1') AS is_seq_start, (last='4' AND next_first='4') AS is_seq_end, *
+             FROM my_cards WHERE (prev_last='9' AND first='1') OR (last='4' AND next_first='4')),
+  candidate_sequencing_cat2 AS (SELECT 'cat2' AS category, cardwidth,
+                       (prev_last='4' AND first='4') AS is_seq_start, (last='9' AND next_first='1') AS is_seq_end, *
+             FROM my_cards WHERE (prev_last='4' AND first='4') OR (last='9' AND next_first='1')),
+  candidate_sequencing AS (SELECT * FROM candidate_sequencing_cat1 UNION ALL SELECT * FROM candidate_sequencing_cat2),
+  candidate_sequences AS (SELECT cand_start.category, cand_start.cardwidth, cand_start.cardnum AS seq_start, cand_end.cardnum AS seq_end
+         FROM candidate_sequencing cand_start INNER JOIN candidate_sequencing cand_end
+        ON cand_start.is_seq_start AND cand_end.is_seq_end AND cand_start.cardnum<=cand_end.cardnum
+          AND cand_start.cardwidth=cand_end.cardwidth AND cand_start.category=cand_end.category),
+ possible_moves AS (SELECT seq.category, ip.cardwidth, seq_start, seq_end, ip.cardnum AS insert_seq_before, ip.cardnum>seq_end AS ip_is_after
+  FROM candidate_sequences seq INNER JOIN candidate_insertpoints ip ON (seq.seq_start=ip.cardnum+1 OR seq.seq_end=ip.cardnum-2)
+     AND ip.cardwidth=seq.cardwidth)
+SELECT *, CASE WHEN ip_is_after THEN insert_seq_before-1 ELSE insert_seq_before END AS move_card,
+       CASE WHEN ip_is_after THEN seq_start ELSE seq_end+1 END AS new_loc
+FROM possible_moves
+ORDER BY move_card;
 
